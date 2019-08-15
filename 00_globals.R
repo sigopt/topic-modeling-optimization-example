c48ea45831597ad4237de62b3bc10a503c8e652d
Tommy Jones <jones.thos.w@gmail.com>
2019-08-14 21:44:33 -0400

    Create a file for global functions and variables

---
+++ 00_globals.R
@@ -0,0 +1,16 @@
+################################################################################
+# This script loads packages and declares functions to be used in the analyses
+################################################################################
+
+### load packages ----
+library(textmineR)
+library(magrittr)
+library(SigOptR)
+
+### declare GLOBAL variables ----
+random_seed <- 8675309
+
+### declare functions ----
+
+
+
