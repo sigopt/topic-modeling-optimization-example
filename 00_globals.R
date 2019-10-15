fd95421d4ecdd94c3b02834b187a6f8b0b938255
Tommy Jones <jones.thos.w@gmail.com>
2019-10-15 15:32:48 -0400

    adding functions

--- 00_globals.R
+++ 00_globals.R
@@ -4,6 +4,7 @@
 
 ### load packages ----
 library(textmineR)
+library(randomForest)
 library(magrittr)
 library(stringr)
 library(SigOptR)
@@ -12,6 +13,23 @@ library(SigOptR)
 random_seed <- 8675309
 
 ### declare functions ----
+
+# Function to train random forest model
+train_classifier <- function(y, x){
+  if (class(x) == "matrix")
+    x <- as.data.frame(x)
+  
+  randomForest(x = x, y = y)
+}
+
+predict_classifier <- function(object, new_data){
+  if (class(new_data) == "matrix")
+    new_data <- as.data.frame(new_data)
+  
+  predict(object = object, newdata = new_data, type = "prob")
+}
+
+# Function to get AUC from the random forest model
 
 
 
